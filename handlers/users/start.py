242129b8c2d54f9beeab87410fb00df5932fe9f4
azamjonga <80978312+azamjonga@users.noreply.github.com>
2022-04-11 02:05:39 +0500

    commitchd

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,9 +1,9 @@
-from aiogram import types
-from aiogram.dispatcher.filters.builtin import CommandStart
+from aiogram.dispatcher.filters import Command, Text
+from aiogram.types import Message, ReplyKeyboardRemove
+from keyboards.default.sozlama import menu
 
 from loader import dp
-
-
-@dp.message_handler(CommandStart())
-async def bot_start(message: types.Message):
-    await message.answer(f"Salom, {message.from_user.full_name}!")
+#from keyboards.inline.languages import button
+@dp.message_handler(Command("start"))
+async def show_menu(message: Message):
+    await message.answer("Assalomalekum men tarjimon botman", reply_markup=menu)
